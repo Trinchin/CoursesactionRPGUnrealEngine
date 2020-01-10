f2f96ff9eb506566019ae4abfded32158fddffed
ivantrinchin <ivan.trinchin@iridescent.studio>
2020-01-10 18:10:13 +0300

    Add project to repository

---
+++ Source/LabEscape/GameplayActors/Floater.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Floater.h"
+
+// Sets default values
+AFloater::AFloater()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CustomStaticMesh"));
+
+	initialLocation = FVector(0.0f, 0.0f, 0.0f);
+	placedLocation = FVector(0.0f, 0.0f, 0.0f);
+	worldOrigin = FVector(0.0f, 0.0f, 0.0f);
+
+	InitialDirection = FVector(0.0f, 0.0f, 0.0f);
+
+	bInitializeFloaterLocations = false;
+	bShouldFloat = false;
+}
+
+// Called when the game starts or when spawned
+void AFloater::BeginPlay()
+{
+	Super::BeginPlay();
+
+	placedLocation = this->GetActorLocation();
+	if (bInitializeFloaterLocations)
+	{
+		this->SetActorLocation(initialLocation);
+	}
+}
+
+// Called every frame
+void AFloater::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bShouldFloat)
+	{
+		FHitResult hitResult;
+		this->AddActorLocalOffset(InitialDirection, true, &hitResult);
+
+		FVector hitLocation = hitResult.Location;
+
+		 
+	}
+
+}
+
