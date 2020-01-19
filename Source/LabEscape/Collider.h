51da4bf8824fcc14f99e046e43fc1dd5a933cfd8
ivantrinchin <ivan.trinchin@iridescent.studio>
2020-01-20 00:22:48 +0300

    Add class Collider Add sphere component to it Add  staticMesh component Create Blueprint iherited from collider class

---
+++ Source/LabEscape/Collider.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+
+#include "Collider.generated.h"
+
+
+UCLASS()
+class LABESCAPE_API ACollider : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollider();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class USphereComponent* SphereComponent;
+
+	FORCEINLINE UStaticMeshComponent* getMeshComponent() { return this->MeshComponent; }
+	FORCEINLINE void setMeshComponent(UStaticMeshComponent* mesh) { this->MeshComponent = mesh; }
+	FORCEINLINE USphereComponent* getSphereComponent() { return this->SphereComponent; }
+	FORCEINLINE void setSphereComponent(USphereComponent* sphere) { this->SphereComponent = sphere; }
+
+};
